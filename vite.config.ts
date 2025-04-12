404b733082ec45a9d1b674bf4e44b4aa59e31639
~Yashh <61193710+Yash-Aanand@users.noreply.github.com>
2025-04-12 01:54:46 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/vortexwings/",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
